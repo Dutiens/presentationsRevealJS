904a7bf5e9e23a0f88d59057ad2a37f1bdadcb00
Thibault Théologien <ttheologien@insa-rouen.fr>
2016-05-26 17:18:46 +0200

    ajout script installation pour demos matlab -> projet stat

---
+++ STATS_MomentsOrdreSuperieur/demos/install.sh
@@ -0,0 +1,16 @@
+if [ ${PWD##*/} != "demos" ]; then
+  echo "Repertoire invalide"
+  exit 1
+fi
+
+if [ ! -d "images" ]; then
+  mkdir images
+fi
+
+if [ ! -f "images/image1.jpg" ]; then
+  wget http://i.imgur.com/ipOaW3L.jpg -O images/image1.jpg
+fi
+
+if [ ! -f "images/image2.jpg" ]; then
+  wget http://i.imgur.com/BRlmJ0V.jpg -O images/image2.jpg
+fi
