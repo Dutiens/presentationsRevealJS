5db4b8d62a79dec2e8c7b021fd68072336592b76
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-05-28 12:28:04 +0200

    ajout d'un script d'importation global

---
+++ import.sh
@@ -0,0 +1,8 @@
+cd ANGLAIS_Workplace/ressources
+bash import.sh
+cd ../../PAO_NAO/ressources
+bash import.sh
+cd ../../STATS_MomentsOrdreSuperieur/ressources
+bash import.sh
+cd ../demos
+bash install.sh
