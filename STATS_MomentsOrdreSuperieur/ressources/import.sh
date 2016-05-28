196f21c01e45e05384a61214225032eaff405d75
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2016-05-28 16:38:02 +0200

    Avancee de la partie ordre 4 -> stats + début reorganisation arborescence

--- STATS_MomentsOrdreSuperieur/ressources/import.sh
+++ STATS_MomentsOrdreSuperieur/ressources/import.sh
@@ -18,3 +18,7 @@ fi
 if [ ! -f "images/image3.jpg" ]; then
   wget http://i.imgur.com/Vm6YPDI.jpg -O images/image3.jpg
 fi
+
+if [ ! -f "images/image4.jpg" ]; then
+  wget http://i.imgur.com/wXwY9xn.jpg -O images/image4.jpg
+fi
